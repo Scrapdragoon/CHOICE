e42a4680aa9692686034a1ea4ccf739f13473cc7
Victor Malone <vm19171@essex.ac.uk>
2021-11-03 18:22:10 +0000

    Made small changes to source packages- specifically, alterations to Editor popup and the addition of a partial basis for the Nide class

--- Capstone Project GUI/src/main/java/DataItems/Node.java
+++ Capstone Project GUI/src/main/java/DataItems/Node.java
@@ -15,16 +15,16 @@ import java.util.ArrayList;
  * This is a class representing a user-created passage. It includes a name, ID, image, and list of links. This will be used to store the data used to create HTML pages based on user input.
  *
  */
-public class Passage {
+public class Node {
 
     private String title;    // name of passage
     private String ID;      // ID of passage (used for searching and whatnot)
     private URL url;        // URL used to access passage
     
     public Image image;                     // accompanying image
-    public ArrayList<Passage> choices;  // list of choices, that connect to other passages
+    public ArrayList<Node> choices;  // list of choices, that connect to other passages/nodes
 
-    public Passage() {
+    public Node() {
         title = "defaultTitle";
 
     }
@@ -49,13 +49,13 @@ public class Passage {
 
     
     //get list of choices
-    public ArrayList<Passage> getChoices() {
+    public ArrayList<Node> getChoices() {
         return choices;
     }
 
     
     // add link to list of choices
-    public void addLink(Passage passage) {
+    public void addLink(Node passage) {
         choices.add(passage);
     }
 
